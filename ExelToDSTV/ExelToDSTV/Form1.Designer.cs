f61d133ab7407169a1591613e17a84d18ec79a3e
Vitaliy3230 <44684547+Vitaliy3230@users.noreply.github.com>
2019-11-09 22:23:23 +0200

    Add files via upload

---
+++ ExelToDSTV/ExelToDSTV/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace ExelToDSTV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.wayFile = new System.Windows.Forms.TextBox();
+            this.Open = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.pathDSTV = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // wayFile
+            // 
+            this.wayFile.Location = new System.Drawing.Point(12, 12);
+            this.wayFile.Name = "wayFile";
+            this.wayFile.Size = new System.Drawing.Size(414, 20);
+            this.wayFile.TabIndex = 0;
+            this.wayFile.Text = "Путь к файлу xlsx";
+            this.wayFile.TextChanged += new System.EventHandler(this.wayFile_TextChanged);
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(432, 12);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(75, 23);
+            this.Open.TabIndex = 1;
+            this.Open.Text = "Open excel";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Save
+            // 
+            this.Save.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Save.Location = new System.Drawing.Point(432, 41);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 34);
+            this.Save.TabIndex = 2;
+            this.Save.Text = "Generate nc1";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // pathDSTV
+            // 
+            this.pathDSTV.Location = new System.Drawing.Point(13, 49);
+            this.pathDSTV.Name = "pathDSTV";
+            this.pathDSTV.Size = new System.Drawing.Size(413, 20);
+            this.pathDSTV.TabIndex = 4;
+            this.pathDSTV.Text = "Путь хранения DSTV";
+            this.pathDSTV.TextChanged += new System.EventHandler(this.pathDSTV_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(380, 39);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Структура Excel:\r\n1й столбец кол-во, 2й - размеры пластины \"6х78х200\", где х-кирр" +
+    "илицой,\r\n3й-марка стали.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 131);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pathDSTV);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Open);
+            this.Controls.Add(this.wayFile);
+            this.Name = "Form1";
+            this.Text = "Генератор DSTV (.nc1) с Екселя";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox wayFile;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.TextBox pathDSTV;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ExelToDSTV/ExelToDSTV/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+using System.Collections;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExelToDSTV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.Filter = "xlsx files(*.xlsx|*.xlsx|All files(*.*)|*.*";                      
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+        }
+
+        void Open_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            string SFN = openFileDialog1.SafeFileName;
+            int ind2 = filename.Length; 
+            int ind = SFN.Length;
+            int s1 = ind2 - ind;
+
+            string dstvPath = filename.Remove(s1);
+            wayFile.Text = filename; //путь файла в тестовое поле
+            pathDSTV.Text = dstvPath; 
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            string filename = wayFile.Text;
+            string pathdstv = pathDSTV.Text;
+            int s1 = 2; //считываемый масив профиля(в ексель А=1,В=2... и т.д.)
+
+            Excel.Application ObjWorkExcel = new Excel.Application(); //открыть эксель
+            
+            Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(@filename, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing); //открыть файл
+            Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1]; //получить 1 лист
+            var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);//1 ячейку
+            //-------------------------------------
+
+            int iLastRow = ObjWorkSheet.Cells[ObjWorkSheet.Rows.Count, "A"].End[Excel.XlDirection.xlUp].Row;  //последняя заполненная строка в столбце А
+            var arrData = (object[,])ObjWorkSheet.Range["A1:Z" + iLastRow].Value;
+           
+            // Инициализируем данный массив
+            ArrayList arrayList = new ArrayList();
+
+            int rows = arrData.GetUpperBound(0);
+            int columns = arrData.Length / rows;
+
+            //перебор строки с размерами пластины
+            for (int i = 1; i < rows+1; i++)
+            {                 
+              arrayList.Add(arrData[i, s1]);
+            }
+
+            //разбиение пластниы
+            ArrayList arrayListPlate = new ArrayList();
+
+            for (int i = 0; i < arrayList.Count; i++)
+            {
+                string x22 = arrayList[i].ToString();
+                string[] x23 = x22.Split(new char[] { 'х' });
+                arrayListPlate.Add(x23);
+            }
+
+            //--------------создание dstv файлов
+            for (int i = 0; i < rows; i++)
+            {
+                string quantity = arrData[i+1, 1].ToString();
+                //габариты детали
+                string[] b = arrayListPlate[i] as string[];
+                
+                string bT = b[0]; //толщина пластины
+                string b2 = b[1]; //ширина пластины
+                string b3 = b[2]; //высота пластины
+
+                string steel = arrData[i+1, 3].ToString();
+                
+                System.IO.StreamWriter dstvFile = new StreamWriter(@pathdstv + "\\" + i + ".nc1");
+                dstvFile.WriteLine("ST");
+                dstvFile.WriteLine($"** {i}.nc1");
+                dstvFile.WriteLine("  1");
+                dstvFile.WriteLine("  1");
+                dstvFile.WriteLine($"  {i}");
+                dstvFile.WriteLine($"  {i}");
+                dstvFile.WriteLine($"  {steel}");
+                dstvFile.WriteLine($"  {quantity}");
+                dstvFile.WriteLine($"  PL{bT}");
+                dstvFile.WriteLine("  B");
+                dstvFile.WriteLine($"     {b2}.00");
+                dstvFile.WriteLine($"     {b3}.00");
+                dstvFile.WriteLine($"      {bT}.00");
+                dstvFile.WriteLine($"      {bT}.00");
+                dstvFile.WriteLine($"      {bT}.00");
+                dstvFile.WriteLine("       0.00");
+                dstvFile.WriteLine("     94.200");
+                dstvFile.WriteLine("      2.144");
+                dstvFile.WriteLine("      0.000");
+                dstvFile.WriteLine("      0.000");
+                dstvFile.WriteLine("      0.000");
+                dstvFile.WriteLine("      0.000");
+                dstvFile.WriteLine("");
+                dstvFile.WriteLine("");
+                dstvFile.WriteLine("");
+                dstvFile.WriteLine("");
+                dstvFile.WriteLine("AK");
+                dstvFile.WriteLine("  v       0.00u      0.00       0.00       0.00       0.00       0.00       0.00");
+                dstvFile.WriteLine($"        {b2}.00       0.00       0.00       0.00       0.00       0.00       0.00");
+                dstvFile.WriteLine($"        {b2}.00     {b3}.00       0.00       0.00       0.00       0.00       0.00");
+                dstvFile.WriteLine($"          0.00     {b3}.00       0.00       0.00       0.00       0.00       0.00");
+                dstvFile.WriteLine("          0.00       0.00       0.00       0.00       0.00       0.00       0.00");
+                dstvFile.WriteLine("EN");
+                dstvFile.Close();
+            }
+            //---------------------------------------------------------
+
+            ObjWorkBook.Close(false, Type.Missing, Type.Missing); //закрыть не сохраняя
+            ObjWorkExcel.Quit(); // выйти из экселя
+            return;
+        }
+
+        private void pathDSTV_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void wayFile_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
